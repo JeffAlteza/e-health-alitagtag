6c17d369341718db860fddd3e0d0dd800628e782
vagrant <vagrant@homestead>
2023-02-02 13:40:31 +0000

    remove vendor to github

--- vendor/filament/filament/resources/views/components/brand.blade.php
+++
@@ -1,9 +0,0 @@
-@if (filled($brand = config('filament.brand')))
-<div @class([ 'filament-brand text-xl font-bold tracking-tight' , 'dark:text-white'=> config('filament.dark_mode'),
-    ])>
-    <div class="flex justify-start">
-        <img src="{{ asset('/Images/logo-rhu.png') }}" alt="Logo" class="h-10">
-        <div class="flex items-center ml-1">{{ $brand }}</div>
-    </div>
-</div>
-@endif
--- vendor/savannabits/filament-flatpickr/config/filament-flatpickr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for Savannabits/Flatpickr
-return [
-    'default_theme' => 'airbnb', // 'default','dark','material_blue','material_green','material_red','material_orange','airbnb','confetti'
-];
